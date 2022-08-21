2c2548731b7c606c1bcc516fec13ce13a06f6516
David SPORN <sporniket.studio@gmail.com>
2022-08-21 10:48:59 +0200

    fix files content and wrong email in library.json for next release

--- include/AnimatedBool.hpp
+++ include/AnimatedBool.hpp
@@ -1,19 +1,19 @@
 // Copyright 2021,2022 David SPORN
 // ---
-// This file is part of 'Kimchi on the beach'.
+// This file is part of 'AnimatedValues'.
 // ---
-// 'Kimchi on the beach' is free software: you can redistribute it and/or 
+// 'AnimatedValues' is free software: you can redistribute it and/or 
 // modify it under the terms of the GNU General Public License as published 
 // by the Free Software Foundation, either version 3 of the License, or 
 // (at your option) any later version.
 
-// 'Kimchi on the beach' is distributed in the hope that it will be useful, 
+// 'AnimatedValues' is distributed in the hope that it will be useful, 
 // but WITHOUT ANY WARRANTY; without even the implied warranty of 
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General 
 // Public License for more details.
 
 // You should have received a copy of the GNU General Public License along 
-// with 'Kimchi on the beach'. If not, see <https://www.gnu.org/licenses/>. 
+// with 'AnimatedValues'. If not, see <https://www.gnu.org/licenses/>. 
 
 // interfaces
 // Animated values library
