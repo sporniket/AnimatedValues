25e6c621d747b0c2738ec0f843f7a06caef8c300
David SPORN <sporniket.studio@gmail.com>
2025-05-06 07:55:16 +0200

    change banner pattern to not be seen by doxygen

--- include/cmspk/animval.hpp
+++ include/cmspk/animval.hpp
@@ -1,12 +1,12 @@
 /* SPDX-License-Identifier: GPL-3.0-or-later */
-/****************************************
+/* ** ** ** ** ** ** ** ** ** ** ** ** **
 
 ---
 Copyright (C) 2021~2025 David SPORN
 ---
 This is part of **Animated values**.
 Created to help you go beyond a simple blinking led.
-****************************************/
+* ** ** ** ** ** ** ** ** ** ** ** ** **/
 
 #ifndef CMSPK__ANIMVAL__HPP
 #define CMSPK__ANIMVAL__HPP
